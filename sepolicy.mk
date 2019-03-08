3e31496a5b585d22f8a6124f4cb36fc4258c3e9c
ix5 <ix5@users.noreply.github.com>
2019-03-08 11:51:15 +0100

    TEMP: Split per_mgr behaviour into legacy/current

--- sepolicy.mk
+++ sepolicy.mk
@@ -14,3 +14,14 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := \
     $(PLATFORM_SDK_VERSION).0
+
+# Legacy devices do not have a vndbinder PeripheralManager
+ifeq ($(BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED),true)
+BOARD_SEPOLICY_M4DEFS += target_legacy_per_mgr=false
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(LOCAL_SEPOLICY)/vendor-current
+else
+BOARD_SEPOLICY_M4DEFS += target_legacy_per_mgr=true
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(LOCAL_SEPOLICY)/private-legacy
+endif
